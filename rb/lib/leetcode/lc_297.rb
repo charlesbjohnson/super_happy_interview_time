c85579d20f30f10073539818c750c0be67698037
Charles B Johnson <mail@cbjohnson.info>
2018-04-06 17:22:12 -0400

    feat(rb/leetcode): add batch of hard solutions

---
+++ rb/lib/leetcode/lc_297.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+module LeetCode
+  # 297. Serialize and Deserialize Binary Tree
+  module LC297
+    TreeNode = Struct.new(:val, :left, :right)
+
+    # Description:
+    # Serialization is the process of converting a data structure or object into a
+    # sequence of bits so that it can be stored in a file or memory buffer,
+    # or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+    #
+    # Design an algorithm to serialize and deserialize a binary tree.
+    # There is no restriction on how your serialization/deserialization algorithm should work.
+    # You just need to ensure that a binary tree can be serialized to a string and
+    # this string can be deserialized to the original tree structure.
+    #
+    # Examples:
+    # Input:
+    #     1
+    #    / \
+    #   2   3
+    #      / \
+    #     4   5
+    # Output: "[1, 2, 3, null, null, 4, 5]"
+    #
+    # Notes:
+    # - You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+    # - Do not use class member/global/static variables to store states. Your serialize and deserialize algorithms should be stateless.
+    #
+    # @param tree {TreeNode}
+    # @return {string}
+    def serialize(tree)
+      queue = [tree]
+      value = []
+
+      until queue.empty?
+        node = queue.shift
+
+        unless node
+          value.push(nil)
+          next
+        end
+
+        value.push(node.val)
+        queue.push(node.left)
+        queue.push(node.right)
+      end
+
+      value.map(&:to_s).join(',').gsub(/,+$/, '')
+    end
+
+    # @param data {String}
+    # @return {TreeNode}
+    def deserialize(data)
+      return if data.empty?
+
+      nodes = data.split(',').map { |s| s.empty? ? nil : TreeNode.new(s.to_i) }
+      nodes.reduce(0) do |i, node|
+        next i unless node
+
+        left = (i * 2) + 1
+        right = left + 1
+
+        node.left = nodes[left] if left < nodes.length
+        node.right = nodes[right] if right < nodes.length
+
+        i + 1
+      end
+
+      nodes.first
+    end
+  end
+end
---
+++ rb/test/leetcode/test_lc_297.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'config'
+require 'leetcode/lc_297'
+
+module LeetCode
+  class TestLC297 < Minitest::Test
+    include LC297
+
+    def build(values)
+      return if values.empty?
+
+      nodes = values.map { |v| v ? TreeNode.new(v) : nil }
+      nodes.each.with_index do |node, i|
+        next unless node
+
+        left = i * 2 + 1
+        right = left + 1
+
+        node.left = nodes[left] if left < nodes.length
+        node.right = nodes[right] if right < nodes.length
+      end
+
+      nodes.first
+    end
+
+    [
+      [[1, 2, 3], '1,2,3'],
+      [[1, 2], '1,2'],
+      [[1, nil, 3], '1,,3'],
+      [[1, nil, 3, nil, nil, nil, 4], '1,,3,,4'],
+      [[1, 2, 3, nil, nil, 4, 5], '1,2,3,,,4,5']
+    ].each.with_index do |(tree, expected), i|
+      define_method(:"test_serialize_deserialize_#{i}") do
+        assert_equal(expected, serialize(build(tree)))
+        assert_equal(build(tree), deserialize(expected))
+      end
+    end
+  end
+end
