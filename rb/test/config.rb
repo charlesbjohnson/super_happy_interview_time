d10ce90070d6520768a222e78eb4d63a830dc274
Charles B Johnson <mail@cbjohnson.info>
2019-05-13 18:43:58 -0500

    feat(rb): setup code coverage

--- rb/test/config.rb
+++ rb/test/config.rb
@@ -3,9 +3,11 @@
 require 'byebug'
 
 require 'minitest/autorun'
+require 'simplecov'
 
 require 'minitest/around/unit'
 require 'minitest/focus'
 require 'minitest/reporters'
 
 Minitest::Reporters.use!(Minitest::Reporters::DefaultReporter.new)
+SimpleCov.start
