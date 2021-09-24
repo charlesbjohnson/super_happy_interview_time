de40ad4358140c9ea01c7f1e11d1d6918e5d335a
Charles B Johnson <mail@cbjohnson.info>
2021-09-24 17:08:52 -0500

    chore(rb): remove dependencies

--- rb/test/config.rb
+++ rb/test/config.rb
@@ -12,8 +12,6 @@ SimpleCov.start {
 require("byebug")
 
 require("minitest/autorun")
-require("minitest/around/unit")
-require("minitest/focus")
 require("minitest/reporters")
 
 Minitest::Reporters.use!(Minitest::Reporters::DefaultReporter.new)
