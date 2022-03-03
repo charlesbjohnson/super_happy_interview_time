67cdb6ba51a35c9683e3c471bc2423afdd53b990
Charles B Johnson <mail@cbjohnson.info>
2022-03-03 00:43:53 -0600

    chore(rb): group dependencies

--- rb/test/config.rb
+++ rb/test/config.rb
@@ -1,15 +1,11 @@
 # frozen_string_literal: true
 
-require("simplecov")
+Bundler.require(:default, :test)
 
 SimpleCov.root(File.expand_path("../", __dir__))
 SimpleCov.start {
   load_profile(:bundler_filter)
   load_profile(:test_frameworks)
 }
-
-require("debug")
-require("awesome_print")
-require("minitest/reporters")
 
 Minitest::Reporters.use!(Minitest::Reporters::DefaultReporter.new)
